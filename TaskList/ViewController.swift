7867b6e56f24d888a12c9397d4a284fa853c8a64
pnastusevich <pasha.nastusevich@gmail.com>
2024-09-14 15:12:13 +0300

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Паша Настусевич on 14.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
